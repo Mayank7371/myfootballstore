c8b48a80ae5a49f34d0779776b1b8525ea156884
Mayank7371 <neprone.dev@gmail.com>
2025-09-06 18:54:20 +0530

    add navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import day from "../../assets/day.png";
+import logo_black from "../../assets/logo-black.png";
+import logo_white from "../../assets/logo-white.png";
+import night from "../../assets/night.png";
+import search_b from "../../assets/search-b.png";
+import search_w from "../../assets/search-w.png";
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-xl text-amber-50">Hi there</h1>
+    </div>
+  );
+};
+
+export default Navbar;
